eae0aeeb6a51cf3cc5e02ee649e35afe533d9e1a
AmandaKhol <amanda.camsua@gmail.com>
2021-04-02 12:57:33 +0200

    add day 2 challenge, clock exercise

---
+++ 02-clock/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const hourHand = document.querySelector('.hour-hand');
+const minHand = document.querySelector('.min-hand');
+const secondHand = document.querySelector('.second-hand');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const hour = now.getHours();
+  const min = now.getMinutes();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const hoursDegrees = (12 / 12) * 360 + 90;
+  const minDegrees = (360 / 60) * min + 90;
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  minHand.style.transform = `rotate(${minDegrees}deg)`;
+  console.log(hour);
+  console.log(secondsDegrees);
+}
+
+setInterval(setDate, 1000);
+
+setDate();
